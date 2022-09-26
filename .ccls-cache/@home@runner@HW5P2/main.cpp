b30a8355af13c8fdd8e54609a4018c6dbb1c19ae
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-09-26 05:34:05 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW5P2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+  int dataTemp;
+  
+  // load input file
+  ifstream inFile; // declare stream variable
+  inFile.open("NumberData.txt");
+
+  // load all data in while loop
+  while (!inFile.eof())
+  {
+    inFile >> dataTemp;
+    cout << dataTemp << endl;
+    
+  }
+}
